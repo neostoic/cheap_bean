35f9482e753c254cd30fa11be433d1e993df1e68
Lance Stacenko <stacenko.lance@yahoo.ca>
2014-02-13 17:07:16 -0500

    wholething

--- ejs_learning/server.js
+++ ejs_learning/server.js
@@ -37,3 +37,45 @@ app.get("/ejs", function(request, response) {
 	response.send(ret);
 });
 app.listen(port);
+
+/* Schema for mongoose. Unsure about support for multiple nested references
+================================
+var nest_schema = mongoose.Schema({
+	company_name: String,
+	website: String,
+	chain: Boolean,
+	date_added: Date,
+	rating: {
+		yelp_rating: Number,
+		yelp_reviews: Number,
+		user_rating: Number,
+		},
+	locations: {
+		location_number: Number,
+		location_name: String,
+		address: String,
+		phone: Number,
+		hours: {
+			Monday: String,
+			Tuesday: String,
+			Wednesday: String,
+			Thursday: String,
+			Friday: String,
+			Saturday: String,
+			Sunday: String,
+			},
+	drinks: {
+		drink: String,
+				{
+				small: Number,
+				medium: Number,
+				large: Number,
+				}
+		}
+});
+
+/* Model for mongoose. Takes schema.
+================================
+var model = mongoose.model('Coffee', nest_schema)
+
+/*
