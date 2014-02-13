b71087b5b6e53fd4c164401329c9f2320f3d4a23
Lance Stacenko <stacenko.lance@yahoo.ca>
2014-02-13 12:45:20 -0500

    Made seperations in logic

---
+++ ejs_learning/mongoose_auth.js
@@ -0,0 +1,14 @@
+/* Mongo datbase connection
+================================*/
+var mongoose = require('mongoose');
+
+// mongodb://heroku:admin@troup.mongohq.com:10075/app22094857
+mongoose.connect('mongodb://heroku:admin@troup.mongohq.com:10075/app22094857');
+
+console.log('Attempting authentication.')
+	var db = mongoose.connection;
+	db.on('error', console.error.bind(console, 'connection error:'));
+	db.once('open', function callback () {
+  		console.log('Yeah were in!');
+	});
+console.log('Over authentication block.')
