89becd2f75fbe18e3d51d38d08b55ebd62b5d76c
Lance Stacenko <stacenko.lance@yahoo.ca>
2014-02-14 16:52:16 -0500

    working version

--- mongoose/1-mongo_auth.js
+++ mongoose/1-mongo_auth.js
@@ -1,7 +1,13 @@
 // Step 1
 // Authenticates into mongo database
+var mongoose = require('mongoose')
+  , Schema = mongoose.Schema
+  , mongo_auth = require('./2-schema_model.js').main
+  , mongo_auth = require('./2-schema_model.js').locations
+  , mongo_auth = require('./2-schema_model.js').drinks
+  , nest_model = require('./2-schema_model.js').nest_model;
 
-var mongoose = require('mongoose');
+var fs = require('fs');
 
 // mongodb://heroku:admin@troup.mongohq.com:10075/app22094857
 mongoose.connect('mongodb://heroku:admin@troup.mongohq.com:10075/app22094857');
@@ -14,6 +20,14 @@ console.log('Attempting authentication.')
 	});
 console.log('Over authentication block.')
 
+nest_model.find({chain: 'true'}, function (err, coffeshop) {
+	 if(err){
+	  onErr(err,callback);
+	 }else{
+	  mongoose.connection.close();
+	  console.log(coffeshop); //Got console.logging to work
+	 }
+	})
+
 //Export authentication so next script can use it.
 module.exports.db = db;
-
