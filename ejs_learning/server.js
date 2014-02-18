90d56728ac0f667d367c85a07881a8e0156ecf62
Lance Stacenko <stacenko.lance@yahoo.ca>
2014-02-18 13:00:05 -0500

    First commit of day. EJS templating is working, displaying data from pushing to var users with users.push. Next steps are to display data from fs file read or display data from database query with mongoose. Figure out how to display specific coffee shops, displaying by index with EJS

--- ejs_learning/server.js
+++ ejs_learning/server.js
@@ -1,111 +1,141 @@
 var ejs = require('ejs')
   , fs = require('fs')
-  , express = require('express')
-  , mongoose = require('mongoose')
-  , Schema = mongoose.Schema
-  , mongo_auth = require('../mongoose/2-schema_model.js').db
-  , db_search = require('../mongoose/1-mongo_auth.js').coffeeshop
-  , nest_model = require('../mongoose/2-schema_model.js').nest_model
-  , app = express;
-
+  , express = require("express");
 
 console.log("starting");
 var port = process.env.PORT || 5000;
 process.env.PWD = process.cwd()
 
-//Serve files out of /public directory. 
+var app = express();
+
+//Serve files out of /public directory.
 app.use(express.static(__dirname + '/public'));
 app.get("/", function(request, response) {
-	response.send("Node.js webserver that serves from ~/Development/Projects/08_frugallycaffeinated/ejs_learning/public");
+  response.send("Welcome to my Node.js webserver");
 });
 
 //Serve /ejs, populated with test data right now.
-app.get("/ejs", function(request, response) {	
-		var path = __dirname + '/functions.ejs'
-		, str = fs.readFileSync(path, 'utf8');
+app.get("/ejs", function(request, response) {
+
+  var path = __dirname + '/functions.ejs'
+  , str = fs.readFileSync(path, 'utf8');
 
 
+  fs.readFile('testdata.js', 'utf8', function (err,data) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log(data);
+    users = data; 
+  })
 
-			/* Declares schemas
-			================================*/
-			var main = Schema({
-			  company_name:        String,
-			  display_name:        String,
-			  website:             String,
-			  chain:               Boolean,
-			  avg_price:           Number, //Most logical place to put avg_price right now
-			  date: {
-			    date_added:       { type: Date, default: Date.now}, //Add logic to differentiate between added and lastupdated
-			    date_lastupdated: { type: Date, default: Date.now},     
-			  },
-			  rating: {
-			    yelp_rating:       Number,
-			    num_reviews:       Number,
-			    user_rating:       Number,
-			  },
-			  locations: [locations],
-			  drinks: [drinks]
-			});
-				/* Schema for embedded subdocument location
-				================================*/
-				var locations = Schema({
-						number:        Number,
-						name:          String,
-						address:       String,
-						phone:         String, 
-						hours: {
-							Monday:    String,
-							Tuesday:   String,
-							Wednesday: String,
-							Thursday:  String,
-							Friday:    String,
-							Saturday:  String,
-							Sunday:    String,
-						}
-				});
-				/* Schema for embedded subdocument drinks
-				================================*/
-				var drinks = Schema({
-						drink:         String,
-						sizes: {
-							small:     Number,
-							medium:    Number,
-							large:     Number,
-					}
-				});
+  //Initialize an empty array
+  var users = [];
+  //Push test data into array
+  users.push({
+          company_name: "Balzac's Coffee Roasters",
+          display_name: "Balzac's",
+          website: "www.balzacs.com",
+          chain: true,
+          avg_price: 2.89,
+          drinks: [
+            {
+              sizes: {
+                large: 3.1,
+                medium: 2.6,
+                small: 2.4
+              },
+              drink: "Americano"
+            },
+            {
+              sizes: {
+                large: 2.8,
+                medium: 2.1,
+                small: 3.4
+              },
+              drink: "Cappucino"
+            }
+          ],
+          locations: [
+            {
+              hours: {
+                Sunday: "8:00 AM - 7:00 PM",
+                Saturday: "7:00 AM - 8:00 PM",
+                Friday: "7:00 AM - 8:00 PM",
+                Thursday: "7:00 AM - 8:00 PM",
+                Wednesday: "7:00 AM - 8:00 PM",
+                Tuesday: "7:00 AM - 8:00 PM",
+                Monday: "7:00 AM - 8:00 PM"
+              },
+              phone: "(416)207-1709",
+              address: "1 Trinity Street, Toronto",
+              location_name: "Distillery District",
+              location_number: 1
+            },
+            {
+              hours: {
+                Sunday: "9:00 AM - 7:00 PM",
+                Saturday: "8:00 AM - 7:00 PM",
+                Friday: "7:00 AM - 7:00 PM",
+                Thursday: "7:00 AM - 7:00 PM",
+                Wednesday: "7:00 AM - 7:00 PM",
+                Tuesday: "7:00 AM - 7:00 PM",
+                Monday: "7:00 AM - 7:00 PM"
+              },
+              phone: "(416)534-7372",
+              address: "43 Hanna Ave, Toronto",
+              location_name: "Liberty Village",
+              location_number: 2
+            },
+            {
+              hours: {
+                Sunday: "10:00 AM - 5:00 PM",
+                Saturday: "10:00 AM - 5:00 PM",
+                Friday: "7:00 AM - 9:00 PM",
+                Thursday: "7:00 AM - 9:00 PM",
+                Wednesday: "7:00 AM - 9:00 PM",
+                Tuesday: "7:00 AM - 9:00 PM",
+                Monday: "7:00 AM - 9:00 PM"
+              },
+              phone: "(416)597-1700",
+              address: "122 Bond Street, Toronto",
+              location_name: "Ryerson Image Arts",
+              location_number: 3
+            },
+            {
+              hours: {
+                Sunday: "9:00 AM - 4:00 PM",
+                Saturday: "8:00 AM - 5:00 PM",
+                Friday: "7:00 AM - 7:00 PM",
+                Thursday: "7:00 AM - 8:30 PM",
+                Wednesday: "7:00 AM - 8:30 PM",
+                Tuesday: "7:00 AM - 8:30 PM",
+                Monday: "7:00 AM - 8:30 PM"
+              },
+              phone: "(416)922-3700",
+              address: "789 Yonge Street, Toronto",
+              location_name: "Toronto Reference Library",
+              location_number: 4
+            }
+          ],
+          rating: {
+            yelp_rating: 3.5,
+            yelp_reviews: 25,
+            user_rating: null
+          },
+          __v: 0});
+  //Log value of users to console
+  console.log(users);
 
-			var coffeeshop = mongoose.model('coffeeshop', main);	
+  var ret = ejs.render(str, {
+  //Map var users
+  users: users,
+  //Set path
+  filename: path
+  });
 
-			//Authenticates into mongo database.
-			mongoose.connect('mongodb://heroku:admin@troup.mongohq.com:10075/app22094857');
-			console.log('Attempting authentication.');
-				var db = mongoose.connection;
-				db.on('error', console.error.bind(console, 'connection error:'));
-				db.once('open', function callback () {
-					console.log('Yeah were in!');
-			});
-			console.log('Over authentication block.');
-
-			//Finds all documents with 'chain: true', and stores callback as var operation.
-			nest_model.find({chain: 'true'}, function (err, coffeeshop) {
-				if(err){
-					onErr(err,callback);
-				}else{
-					mongoose.connection.close();
-					operation = coffeeshop;
-					console.log('Stored coffeeshop callback as var operation, and closed mongo DB connection');
-				}
-			});
-
-
-		db_search = users;
-		console.log(users);
-
-		var ret = ejs.render(str, {
-			users: users,
-			filename: path
-		});
-
-	response.send(ret);
+  response.send(ret);
 });
 app.listen(port);
+
+
