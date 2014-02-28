0c71aafe1e3a2fecf458362402617cf5c45f0942
Lance Stacenko <stacenko.lance@yahoo.ca>
2014-02-28 17:30:24 -0500

    Added logic to iterate through address_components instead of manually indexing

--- working_files/mongoose_scripts/encodeURI_coords.js
+++ working_files/mongoose_scripts/encodeURI_coords.js
@@ -9,6 +9,7 @@ var request = require('request');
 addresses = ['426 College St, Toronto, ON M5T 1T3', '43 Hanna Ave #123, Toronto, ON M6K 1X1', '215 Spadina Ave Toronto, ON'];
 
 //Bounding box, Southest|Northeast coords. Restricts results to only Toronto
+//Find way to concatinate as parameter.
 bounds = "-79.025345,43.591134|-79.709244,43.811540"; //Selected southeast corner below torontoislands/scarborough, northwest markham/brampton
 
 //for loop that iterates through addresses locations and returns formatted URL for each.
@@ -16,27 +17,37 @@ for (i=0; i < addresses.length; i++){
 	var address = addresses[i];
 	var url = 'http://maps.googleapis.com/maps/api/geocode/json?sensor=false&address=' + encodeURIComponent(address);
 	console.log(url);
+
+	//Makes http request to each URL returned by for loop.
+	request(url, function (error, response, body) {
+		if (!error) {
+			var json = JSON.parse(body);
+				//-----COORDS---------
+				var lat =				(json.results[0].geometry.location.lat); //Latitide
+				var lng =				(json.results[0].geometry.location.lng); //Longitude
+				var formatted_address = (json.results[0].formatted_address); //formatted_address
+
+				//-----SPECIFICS------ Parsing API result.
+				var street_number = 	(json.results[0].address_components[0].short_name); //street_number
+				var street_name = 		(json.results[0].address_components[1].short_name); //'route'
+				var neighbourhood = 	(json.results[0].address_components[2].short_name); //'neighbourhood'
+				var city = 				(json.results[0].address_components[4].short_name);
+				var provence = 			(json.results[0].address_components[6].short_name);
+				var country =			(json.results[0].address_components[7].long_name); //Country 'Canada'
+
+					//-----CONCAT--------- Concatinated results
+					var coords = [lat, lng]; //Array of formatted coordinates, Lat/Lng
+					var address = street_number +" "+ street_name +", "+ neighbourhood +", "+ city +", "+ provence +", "+ country;
+					console.log(coords);
+
+			//Conor suggested writing flow control that iterates through address_components, searching by 
+			//types. For example, if 'types' = locality, assume city name, as indexing using positioning
+			//is not repeatable and reliable.
+			for (i=0; i< json.results[0].address_components.length; i++) {
+				console.log(i);
+			}
+		}
+	});
 }
-
-request(url, function (error, response, body) {
-	if (!error) {
-		var json = JSON.parse(body);
-			//-----COORDS---------
-			var lat =				(json.results[0].geometry.location.lat); //Latitide
-			var lng =				(json.results[0].geometry.location.lng); //Longitude
-
-			//-----SPECIFICS------ Parsing API result.
-			var formatted_address = (json.results[0].formatted_address); //formatted_address
-			var street_number = 	(json.results[0].address_components[0].short_name); //street_number
-			var street_name = 		(json.results[0].address_components[1].short_name); //'route'
-			var neighbourhood = 	(json.results[0].address_components[2].short_name); //
-			var city = 				(json.results[0].address_components[4].short_name);
-			var provence = 			(json.results[0].address_components[6].short_name);
-			var country =			(json.results[0].address_components[7].long_name); //Country 'Canada'
-
-			var coords = [lat, lng]; //Array of formatted coordinates, Lat/Lng
-			console.log(coords);
-	}
-});
 
 //Return latitude & longitude to mongo database with save operation under modified schema, here!
