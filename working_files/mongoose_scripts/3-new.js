1c75293fc7a0fc5048cbaf4cb71f4c14f7738ef9
Lance Stacenko <stacenko.lance@yahoo.ca>
2014-02-28 12:42:13 -0500

    Cleaned up 3-new.js, template. Now blank for easy error reduced data entry

--- working_files/mongoose_scripts/3-new.js
+++ working_files/mongoose_scripts/3-new.js
@@ -1,103 +1,56 @@
 /* Mongo datbase connection
 ================================*/
-var mongoose = require('mongoose')
-  , Schema = mongoose.Schema
-  , mongo_auth = require('./2-schema_model.js')
-  , nest_model = require('./2-schema_model.js').nest_model;
-
-  console.log(mongo_auth);
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var mongo_auth = require('./2-schema_model.js');
+var nest_model = require('./2-schema_model.js').nest_model;
   
 /* Data entry into model
 ================================*/
 var coffeeshop = new nest_model({
-	company_name: "Balzac's Coffee Roasters",
-	display_name: "Balzac's",
-	website: 'http://www.balzacs.com',
-	chain: true,
-	avg_price: 2.86,
+	company_name: '',
+	display_name: '',
+	website: '',
+	chain: false,
+	avg_price: 0.00,
 	date: {
 		date_added: { type: Date, default: Date.now}, //come up with right logic
 		date_lastupdated: { type: Date, default: Date.now},		
 	},
 	rating: {
-		yelp_rating: 3.5,
-		yelp_reviews: 25,
+		yelp_rating: 0.0,
+		yelp_reviews: null,
 		user_rating: null,
 	},
 	locations: [{
 		number: 1,
-		name: 'Distillery District',
-		address: '1 Trinity Street, Toronto',
-		phone: '(416)207-1709', 
+		name: '',
+		address: '',
+		phone: '', 
 		hours: {
-			Monday: '7:00 AM - 8:00 PM',
-			Tuesday: '7:00 AM - 8:00 PM',
-			Wednesday: '7:00 AM - 8:00 PM',
-			Thursday: '7:00 AM - 8:00 PM',
-			Friday: '7:00 AM - 8:00 PM',
-			Saturday: '7:00 AM - 8:00 PM',
-			Sunday: '8:00 AM - 7:00 PM',
+			Monday: '0:00 AM - 0:00 PM',
+			Tuesday: '0:00 AM - 0:00 PM',
+			Wednesday: '0:00 AM - 0:00 PM',
+			Thursday: '0:00 AM - 0:00 PM',
+			Friday: '0:00 AM - 0:00 PM',
+			Saturday: '0:00 AM - 0:00 PM',
+			Sunday: '0:00 AM - 0:00 PM',
 		},
-	},
-	{
-		number: 2,
-		name: 'Liberty Village',
-		address: '43 Hanna Ave, Toronto',
-		phone: '(416)534-7372',
-		hours: {
-			Monday: '7:00 AM - 7:00 PM',
-			Tuesday: '7:00 AM - 7:00 PM',
-			Wednesday: '7:00 AM - 7:00 PM',
-			Thursday: '7:00 AM - 7:00 PM',
-			Friday: '7:00 AM - 7:00 PM',
-			Saturday: '8:00 AM - 7:00 PM',
-			Sunday: '9:00 AM - 7:00 PM',
-		},
-	},
-	{
-		number: 3,
-		name: 'Ryerson Image Arts',
-		address: '122 Bond Street, Toronto',
-		phone: '(416)597-1700',
-		hours: {
-			Monday: '7:00 AM - 9:00 PM',
-			Tuesday: '7:00 AM - 9:00 PM',
-			Wednesday: '7:00 AM - 9:00 PM',
-			Thursday: '7:00 AM - 9:00 PM',
-			Friday: '7:00 AM - 9:00 PM',
-			Saturday: '10:00 AM - 5:00 PM',
-			Sunday: '10:00 AM - 5:00 PM',
-		}
-	},
-	{
-		number: 4,
-		name: 'Toronto Reference Library',
-		address: '789 Yonge Street, Toronto',
-		phone: '(416)922-3700',
-		hours: {
-			Monday: '7:00 AM - 8:30 PM',
-			Tuesday: '7:00 AM - 8:30 PM',
-			Wednesday: '7:00 AM - 8:30 PM',
-			Thursday: '7:00 AM - 8:30 PM',
-			Friday: '7:00 AM - 7:00 PM',
-			Saturday: '8:00 AM - 5:00 PM',
-			Sunday: '9:00 AM - 4:00 PM',
-		}
 	}],
 	drinks: [{
-		drink: 'Americano',
+		drink: '',
 			sizes: {
-				small: 2.40,
-				medium: 2.60,
-				large: 3.10,
+				small: 0.00,
+				medium: 0.00,
+				large: 0.00,
 			}
 		},
 		{
-		drink: 'Cappucino',
+		drink: '',
 			sizes: {
-				small: 3.40,
-				medium: 2.10,
-				large: 2.80,
+				small: 0.00,
+				medium: 0.00,
+				large: 0.00,
 			}		
 		}]
 
