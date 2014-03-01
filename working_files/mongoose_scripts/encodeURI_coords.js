2a2c81b31ad2290a85ccf35bf4c66dc212215afe
Lance Stacenko <stacenko.lance@yahoo.ca>
2014-03-01 00:23:57 -0500

    Cleaned up node script

--- working_files/mongoose_scripts/encodeURI_coords.js
+++ working_files/mongoose_scripts/encodeURI_coords.js
@@ -1,67 +1,49 @@
-//Script to authenticate into mongo DB, pull whole database, and for each coffeeshop
-//with empty lat&long fields, run address through google maps geocoding APi and save
-//result in database under lat&long fields.
-var request = require('request');
+//Node.js script for CheapBean.com, takes addresses as input, formats addresses with googlemaps geocoding API,
+//then returns an array of coordinates, in addition to other parameters that may be used later in the project,
+//such as neighbourhood names and short/long hand abbreviations for addresses. Will add support for mongo DB
+//read/write operations, to update records with coordinates automatically.
 
-//Write control flow here that iterates through mongo dataset, and returns each address, populating array 'addresses'
+//Write control flow here that iterates through mongo dataset, and returns each address,
+//populating array 'testdata' in lieu of manually adding.
 
-//Array holding addresses of coffee shops (Eventually populating with DB result)
-addresses = ['426 College St, Toronto, ON M5T 1T3', '43 Hanna Ave #123, Toronto, ON M6K 1X1', '215 Spadina Ave Toronto, ON'];
+//Array holding testdata of coffee shops (Eventually populating with DB result)
+testdata = ['426 College St, Toronto, ON M5T 1T3', '43 Hanna Ave #123, Toronto, ON M6K 1X1', '215 Spadina Ave Toronto, ON'];
 
 //Google maps geocoding API parameters.
-	//Region parameter. (ca = Canada)
-		region = 'ca';
-	//Bounding box, Southest|Northeast coords. Restricts results to GTA only.
-	//Selected southeast corner below torontoislands/scarborough, northwest markham/brampton.
-		bounds = '-79.025345,43.591134|-79.709244,43.811540'; 
-	//API key, geolocation and gmaps V3 enabled
-		apikey = 'AIzaSyC-CIhgJ0CGbhGAOQBmW67H1p0Y_20lXGg';
-	//Location sensor
-		sensorstatus = 'false'
+region = 'ca'; //Region parameter. (ca = Canada)
+bounding_box = '-79.025345,43.591134|-79.709244,43.811540'; //Selected southeast corner below Toronto islands/scarborough, northwest markham/brampton. (GTA)
+apikey = 'AIzaSyC-CIhgJ0CGbhGAOQBmW67H1p0Y_20lXGg';	//API key, geolocation and gmaps V3 enabled
+sensorstatus = 'false' //Location sensor, false
 
-//for loop that iterates through addresses locations and returns formatted URL for each.
-for (i=0; i < addresses.length; i++){
-	var address = addresses[i];
+//for loop that iterates through array locations and returns formatted URL for each.
+for (i=0; i < testdata.length; i++){
+	var address = testdata[i];
 	var url = 'http://maps.googleapis.com/maps/api/geocode/json'
 				+ '?sensor='+  sensorstatus
 				+ '&region='+  region
-				+ '&bounds='+  bounds
+				+ '&bounds='+  bounding_box
 				+ '&address='+ encodeURIComponent(address);
-	//console.log(url);
 
-	//Makes http request to each URL returned by for loop.
+	//NPM dependancy 'require'
+	var request = require('request');
+	//HTTP request to each formatted URL returned by for loop
 	request(url, function (error, response, body) {
 		if (!error) {
 			var json = JSON.parse(body);
-				//-----COORDS---------
+				//-----Fundimental-----
 				var lat =				(json.results[0].geometry.location.lat); //Latitide
 				var lng =				(json.results[0].geometry.location.lng); //Longitude
 				var formatted_address = (json.results[0].formatted_address); //formatted_address
-
-				//-----SPECIFICS------ Parsing API result.
-				var street_number = 	(json.results[0].address_components[0].short_name); //street_number
-				var street_name = 		(json.results[0].address_components[1].short_name); //'route'
-				var neighbourhood = 	(json.results[0].address_components[2].short_name); //'neighbourhood'
-				var city = 				(json.results[0].address_components[4].short_name);
-				var provence = 			(json.results[0].address_components[6].short_name);
-				var country =			(json.results[0].address_components[7].long_name); //Country 'Canada'
-
-					//-----CONCAT--------- Concatinated results
+					//-----CONCAT-----
 					var coords = [lat, lng]; //Array of formatted coordinates, Lat/Lng
-					var address = street_number +" "+ street_name +", "+ neighbourhood +", "+ city +", "+ provence +", "+ country;
-					console.log(coords);
+					console.log(coords); //Log out to console as a demonstration
 
-			//Conor suggested writing flow control that iterates through address_components, searching by 
-			//types. For example, if 'types' = locality, assume city name, as indexing using positioning
-			//is not repeatable and reliable.
-
-
-			// Function takes 3 paramenters, components is array of address components, type is key of address component to be returned
-			// is_long is boolean, true or false, dependant on result.
+			//Function takes 3 paramenters, components is array of address components, type is key of address component to be returned
+			//is_long is boolean, true or false, dependant on result.
 			function getAddressComponent(components, type, is_long){
 				//Setting up a loop to iterate through address components in array
-				for(var i =0; i < components.length; i++){
-					var component = components[i];
+				for(var n =0; n < components.length; n++){
+					var component = components[n];
 					//Iterating through types array in component
 					for(var j = 0; j < component.types.length; j++){
 						if( component.types[j] == type){ //Checking to see if type that we want is in this array, if so returning long or short name based on boolean.
@@ -78,4 +60,4 @@ for (i=0; i < addresses.length; i++){
 	});
 }
 
-//Return latitude & longitude to mongo database with save operation under modified schema, here!
+//Return latitude & longitude to mongo database with save operation under modified schema, here.
